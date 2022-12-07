47380ab4ffd2180c22d39de0dc7aa46af2b2bb03
Seo JeongSeop <seo92js@gmail.com>
2022-12-08 01:54:14 +0900

    HelloController, HelloResponsDto

---
+++ src/main/java/com/seojs/springbootwebservice/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.seojs.springbootwebservice.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
